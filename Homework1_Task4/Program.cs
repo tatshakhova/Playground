165060cf975ad764cb100732f3b6c9ee7629d42f
Tati <tatshakhova@gmail.com>
2022-10-28 15:58:49 +0300

    Complete Homework 1 Task 4

---
+++ Homework1_Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число:");
+int x=Convert.ToInt32 ( Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int y=Convert.ToInt32 ( Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int z=Convert.ToInt32 ( Console.ReadLine());
+if  (x>y && x>z)
+{
+   Console.WriteLine("Большее число: "+x);
+}
+else if (y>x && y>z)
+{
+    Console.WriteLine("Большее число: "+y);
+}
+
+else
+{
+    Console.WriteLine("Большее число: "+z);
+}
