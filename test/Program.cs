dd32e2baaef42e2e2f6957acaed2e687e7e08da7
Tati <tatshakhova@gmail.com>
2022-11-01 02:07:10 +0300

    test

---
+++ test/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int x=Convert.ToInt32(Console.ReadLine());
+int x1=x/10000;
+int x2=x%10000/1000;
+int x4=x%100/10;
+int x5=x%1000%10;
+if ((x1==x5) && (x2==x4))
+    Console.WriteLine("Да, это палиндром!");
+else
+    Console.WriteLine("Нет, это не палиндром!");
+
