1d04edd71c648681545ab7972186afdc91fa60a4
Tati <tatshakhova@gmail.com>
2022-11-01 02:03:34 +0300

    test with gitignore

--- Task3_19/Program.cs
+++ Task3_19/Program.cs
@@ -5,6 +5,6 @@ int x2=x%10000/1000;
 int x4=x%100/10;
 int x5=x%1000%10;
 if ((x1==x5) && (x2==x4))
-    Console.WriteLine("Да, это палиндром.");
+    Console.WriteLine("Да, это палиндром!");
 else
-    Console.WriteLine("Нет, это не палиндром.");
+    Console.WriteLine("Нет, это не палиндром!");
