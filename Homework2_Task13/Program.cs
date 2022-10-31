e8abf9ed5b197a7704a37d2cc52d3bcf00d87b5f
Tati <tatshakhova@gmail.com>
2022-11-01 01:47:15 +0300

    all

---
+++ Homework2_Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int x=Convert.ToInt32(Console.ReadLine());
+if ((x>99) && (x<1000))
+{
+    Console.WriteLine($"Третья цифра введенного числа: {x%10}");
+}
+else if (x>1000)
+{
+    while (x>1000)
+       {
+        x=x/10;
+       } 
+    Console.WriteLine($"Третья цифра введенного числа: {x%10}");
+}
+else Console.WriteLine("Третьей цифры нет :(");
---
+++ Homework2_Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число от 1 до 7:");
+int x=Convert.ToInt32(Console.ReadLine());
+if (x==6||x==7)
+{
+    Console.WriteLine("Это выходной день.");
+}
+else if (x>=1 && x<=5)
+{
+    Console.WriteLine("Это рабочий день.");
+}
+else Console.WriteLine("Такого дня недели нет.");
