4917235fd382fb0163e512423ce9293b987473f9
Tati <tatshakhova@gmail.com>
2022-11-02 17:05:28 +0300

    Complete Task 25

---
+++ Homework4_Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = ReadInt("Введите число A: ");
+int b = ReadInt("Введите число B: ");
+Degree(a, b);
+
+void Degree(int x, int y)
+{
+    int result = 1;
+    for (int i = 1; i <= y; i++)
+    {
+        result = result * x;
+    }
+    Console.WriteLine(result);
+}
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
