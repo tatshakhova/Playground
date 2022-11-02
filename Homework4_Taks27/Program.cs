ed40fa34bc6c5a68ad60d50ee1aa6e10bb73c905
Tati <tatshakhova@gmail.com>
2022-11-03 00:25:53 +0300

    Complete Task 27

---
+++ Homework4_Taks27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+SumNumbers(num);
+
+void SumNumbers(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+Console.WriteLine(sum);
+}
