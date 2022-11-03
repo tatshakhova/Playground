c9df0f6ccb0ff4d3ade8609becf1619b80859583
Tati <tatshakhova@gmail.com>
2022-11-03 17:48:20 +0300

    Complete Task 29

---
+++ Homework4_Task29/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = new int[8];
+
+for (int i=0; i<8; i++)
+    {
+        Console.WriteLine("Введите значение элемента массива: ");
+        array[i] = Convert.ToInt32(Console.ReadLine()); 
+    }
+
+printarray(array);
+
+void printarray(int[] arrayname)
+{
+    for (int i=0; i<8; i++)
+    {
+        Console.Write(arrayname[i]+" ");
+    }   
+}
+
+
