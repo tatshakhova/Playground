bf30f86b061188a4e189e9dfe653456fe2de96e8
Tati <tatshakhova@gmail.com>
2022-11-04 14:30:06 +0300

    Complete Task 36

---
+++ Homework5_Task36/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+Console.WriteLine("Введите количество элементов массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArray()
+{
+    int[] array = new int[n];
+
+    for(int i = 0; i < n; i++)
+    {   
+        array[i] = new Random().Next(1, 100);
+        Console.WriteLine($"{array[i]} ");
+    }
+    return array;
+}     
+ 
+void Summa(int[] array)
+{
+    
+    int sum = 0;
+    for(int i = 0; i < array.Length; i=i+2)
+    {
+        sum = array[i] + sum;
+    }
+
+    Console.WriteLine($"Сумма элементов на нечетных позициях: {sum}");
+}
+
+int[] mas = CreateArray();
+Summa(mas);
