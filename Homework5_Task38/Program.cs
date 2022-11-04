8614a59d81ca4902738a9e618114efcb0e47ad5b
Tati <tatshakhova@gmail.com>
2022-11-04 14:31:14 +0300

    Complete Task 38

---
+++ Homework5_Task38/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите количество элементов массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Массив: ");
+
+double[] CreateArray(int n)
+{
+    double[] array = new double[n];
+
+    for(int i = 0; i < n; i++)
+    {   
+        double a = new Random().Next(1, 10);
+        array[i] = new Random().NextDouble() + a;
+        Console.WriteLine($"{array[i]} ");
+    }
+    return array;
+}     
+ 
+void Difference(double[] array)
+{  
+    double max = array[0];
+    double min = array[0];
+
+    for(int i = 0; i < array.Length; i++)
+    {   
+        if (array[i] >= max) max = array[i];
+        if (array[i] <= min) min = array[i];
+    }     
+    Console.WriteLine($"Максимум: {max}");
+    Console.WriteLine($"Минимум: {min}");
+    Console.WriteLine($"Разница: {max - min}");
+}
+
+
+double[] mas = CreateArray(n);
+Difference(mas);
