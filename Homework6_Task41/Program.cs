0b25112efb7722b9217959502a0013ef9a83249d
Tati <tatshakhova@gmail.com>
2022-11-07 13:05:39 +0300

    Complete Task 41

---
+++ Homework6_Task41/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine()); 
+
+int[] array = new int[m];
+
+for (int i=0; i<m; i++)
+    {
+        Console.WriteLine("Введите значение элемента массива: ");
+        array[i] = Convert.ToInt32(Console.ReadLine()); 
+    }
+
+Console.WriteLine("Массив: ");
+printarray(array);
+Console.WriteLine(" ");
+
+void printarray(int[] arrayname)
+{
+    for (int i=0; i<m; i++)
+    {
+        Console.Write(arrayname[i]+" ");
+    }   
+}
+
+Count(array);
+
+void Count(int[] array)
+{
+    
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+
+    Console.WriteLine($"Количество элементов больше нуля: {count}");
+}
+
+
+
