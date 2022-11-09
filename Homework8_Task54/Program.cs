eb2428bb07159d90b341a6e051e633509db92231
Tati <tatshakhova@gmail.com>
2022-11-09 21:42:23 +0300

    Complete Task 54

---
+++ Homework8_Task54/Program.cs
@@ -0,0 +1,48 @@
+﻿int[,] mas = new int[4, 4];
+FillArray(mas);
+PrintArray(mas);
+SortMaxMin(mas);
+Console.WriteLine();
+PrintArray(mas);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortMaxMin(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int h = 0; h < array.GetLength(1) - 1; h++)
+            {
+                if (array[i, h] < array[i, h + 1])
+                {
+                    int temp = array[i, h + 1];
+                    array[i, h + 1] = array[i, h];
+                    array[i, h] = temp;
+                }
+            }
+        }
+    }
+}
