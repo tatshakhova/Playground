5145552f38ded06c82e2c30b583b770919ead52b
Tati <tatshakhova@gmail.com>
2022-11-10 22:44:27 +0300

    Complete 64

---
+++ Homework9_Task64/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string Rec(int n)
+{
+if (n == 0) return "";
+return Convert.ToString(n) + " " + Rec(n-1);
+}
+
+Console.WriteLine(Rec(n));
+
