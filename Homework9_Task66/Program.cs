e2faefe168c4c692f68de8cb9d9385b87a599ed5
Tati <tatshakhova@gmail.com>
2022-11-10 23:06:38 +0300

    Complete Task 66

---
+++ Homework9_Task66/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Rec(int m)
+{
+if (m == n + 1) return 0;
+return m + Rec(m + 1);
+}
+
+Console.WriteLine(Rec(m));
