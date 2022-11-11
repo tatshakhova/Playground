0d1281150cb1d679ee1cf5526ccde94d0e3c603a
Tati <tatshakhova@gmail.com>
2022-11-11 13:36:59 +0300

    Complete Task 68

---
+++ Homework9_Task68/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write ("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Function (int m, int n)
+{
+    if (m == 0) 
+    return n + 1;
+
+    if (m > 0 && n == 0) 
+    return Function(m - 1, 1);
+
+    if (m > 0 && n > 0) 
+    return Function(m - 1, Function(m, n - 1));
+
+return Function(m, n);
+}
+
+Console.WriteLine($"Значение функции Аккермана: {Function(m, n)}");
